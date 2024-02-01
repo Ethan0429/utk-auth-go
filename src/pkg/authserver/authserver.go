3a2c6f0099342e413cab4f657a78398dd35a88ab
Ethan0429 <ethanrickert0429@gmail.com>
2024-02-01 16:54:30 -0500

    HTTP server now only responds in valid JSON API response

--- src/pkg/authserver/authserver.go
+++ src/pkg/authserver/authserver.go
@@ -1,7 +1,6 @@
 package authserver
 
 import (
-  "sync"
 	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
@@ -12,10 +11,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
 
 var mutex sync.Mutex
 var session *discordgo.Session
+
+type ApiResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
 
 func init() {
 	// setup dotenv
@@ -50,7 +56,8 @@ func GenerateUserTokenHandler(w http.ResponseWriter, r *http.Request) {
 
 	authHeader := r.Header.Get("X-Custom-Auth")
 	if authHeader != sharedSecret {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Unauthorized"})
+		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
@@ -58,13 +65,14 @@ func GenerateUserTokenHandler(w http.ResponseWriter, r *http.Request) {
 	guildDiscordID := r.URL.Query().Get("guild-discord-id")
 
 	if userDiscordID == "" || guildDiscordID == "" {
-		http.Error(w, "Missing parameters", http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Missing parameters"})
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	// check if id already exists
 	{
-    mutex.Lock()
+		mutex.Lock()
 		file, err := ioutil.ReadFile("/data/tokens.json")
 		if err != nil {
 			if !os.IsNotExist(err) {
@@ -82,17 +90,18 @@ func GenerateUserTokenHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		}
-    mutex.Unlock()
+		mutex.Unlock()
 	}
 
 	token, err := generateToken()
 	if err != nil {
-		http.Error(w, "Error generating token", http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Error generating token"})
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	// Read the existing tokens
-  mutex.Lock()
+	mutex.Lock()
 	file, err := ioutil.ReadFile("/data/tokens.json")
 	if err != nil {
 		if !os.IsNotExist(err) {
@@ -108,7 +117,7 @@ func GenerateUserTokenHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error parsing file", http.StatusInternalServerError)
 		return
 	}
-  mutex.Unlock()
+	mutex.Unlock()
 
 	// Add or update the token for the user
 	tokens[userDiscordID] = TokenData{Token: token, GuildID: guildDiscordID}
@@ -120,35 +129,37 @@ func GenerateUserTokenHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  mutex.Lock()
+	mutex.Lock()
 	err = ioutil.WriteFile("/data/tokens.json", updatedData, 0644)
 	if err != nil {
 		http.Error(w, "Error writing to file", http.StatusInternalServerError)
 		return
 	}
-  mutex.Unlock()
+	mutex.Unlock()
 
-	response := TokenResponse{Token: token}
+	response := ApiResponse{Success: true, Message: "Token generated successfully", Data: TokenResponse{Token: token}}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
 // Handler for verifying user token
 func VerifyHandler(w http.ResponseWriter, r *http.Request) {
-  if r.Method != "POST" {
-    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    log.Println("POST method required to verify")
-    return
-  }
+	if r.Method != "POST" {
+		json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Method not allowed"})
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	userDiscordID := r.URL.Query().Get("user-discord-id")
 	token := r.URL.Query().Get("token")
 
 	if userDiscordID == "" || token == "" {
-		http.Error(w, "Missing parameters", http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Missing parameters"})
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-  mutex.Lock()
+	mutex.Lock()
 	file, err := ioutil.ReadFile("/data/tokens.json")
 	if err != nil {
 		http.Error(w, "Error reading file", http.StatusInternalServerError)
@@ -161,11 +172,12 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error parsing file", http.StatusInternalServerError)
 		return
 	}
-  mutex.Unlock()
+	mutex.Unlock()
 
 	if tokenData, ok := tokens[userDiscordID]; ok {
 		if tokenData.Token == token {
-			fmt.Fprintln(w, "Verification successful")
+			log.Println("Verification successful")
+			json.NewEncoder(w).Encode(ApiResponse{Success: true, Message: "Verification successful"})
 
 			// add role to user
 			log.Printf("Adding role for -\n"+
@@ -174,13 +186,13 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 				"   Role ID: %s\n",
 				userDiscordID, tokens[userDiscordID].GuildID, os.Getenv("AUTH_ROLE_ID"))
 
-      mutex.Lock()
+			mutex.Lock()
 			err := session.GuildMemberRoleAdd(tokens[userDiscordID].GuildID, userDiscordID, os.Getenv("AUTH_ROLE_ID"))
 			if err != nil {
 				log.Println("Error adding roll to user:", err)
 			}
-      log.Println("Role added successfully")
-      mutex.Unlock()
+			log.Println("Role added successfully")
+			mutex.Unlock()
 
 			// remove key from map
 			delete(tokens, userDiscordID)
@@ -191,15 +203,16 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
 			}
 
-      mutex.Lock()
+			mutex.Lock()
 			err = ioutil.WriteFile("/data/tokens.json", updatedData, 0644)
-      mutex.Unlock()
+			mutex.Unlock()
 
 			if err != nil {
 				http.Error(w, "Error writing to file", http.StatusInternalServerError)
 			}
 		} else {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(ApiResponse{Success: false, Message: "Invalid token"})
+			w.WriteHeader(http.StatusUnauthorized)
 		}
 	} else {
 		http.Error(w, "User not found", http.StatusNotFound)
